963fb33745257d08687600942ee4b710d116a347
caio_th <caio_th@yahoo.com.br>
2012-12-31 01:06:39 +0000

    Comments to StreamMath

--- engine/StreamMath.cpp
+++ engine/StreamMath.cpp
@@ -1,88 +1,150 @@
-
-/****************************************************************************
- **
- ** Copyright (C) 2010 Victor Algaba .
- ** All rights reserved.
- ** Contact: (www.radit.org)
- **
- ** *************************************************************************/
-
-
-
+/**
+ * StreamMath
+ * -----------------------------------------
+ *
+ * - This class supports the mathematical system of the audio file.
+ *
+ * @author Victor Algaba
+ */
 #include <math.h>
 #include <QTime>
 #include <QDebug>
 #include "StreamMath.h"
 
-StreamMath::StreamMath()
-{
-}
+StreamMath::StreamMath(){}
 
 StreamMath::StreamMath(HSTREAM stream)
 {
     this->stream=stream;
-
 }
 
-
-
-void StreamMath::SetStream(HSTREAM stream){
- this->stream=stream;
-
+/**
+ * Set Hstream
+ * @brief StreamMath::SetStream
+ * @param stream
+ * @return void
+ */
+void StreamMath::SetStream(HSTREAM stream)
+{
+    this->stream=stream;
 }
 
-
-
-
-double StreamMath::Duracion(){
+/**
+ * Get Duration
+ * @brief StreamMath::Duracion
+ * @return double
+ */
+double StreamMath::Duracion()
+{
     return(BASS_ChannelBytes2Seconds(stream, BASS_ChannelGetLength(stream, BASS_POS_BYTE)));
 }
 
-double StreamMath::Trascurridos(){
+/**
+ * Get time in progress
+ * @brief StreamMath::Trascurridos
+ * @return double
+ */
+double StreamMath::Trascurridos()
+{
     return(BASS_ChannelBytes2Seconds(stream, BASS_ChannelGetPosition(stream, BASS_POS_BYTE)));
 }
 
-
-
-double StreamMath::Contador(){
+/**
+ * Returns the duration in seconds elapsed minus
+ * (Duration less Transcurridos)
+ * @brief StreamMath::Contador
+ * @return double
+ */
+double StreamMath::Contador()
+{
     return(Duracion()-Trascurridos());
 }
 
+/**
+ * Get level of DB of channel right
+ * @brief StreamMath::VumetroIz
+ * @return double
+ */
+double StreamMath::VumetroDe()
+{
+    //double level;
+    int level;
+    level=BASS_ChannelGetLevel(stream);
+    return(ToDb(LOWORD(level)));
+}
 
-///////////////////////////////////////////////////////
-QString StreamMath::SegundoToFormato(QString Formato){
+/**
+ * Get level of DB of channel left
+ * @brief StreamMath::VumetroIz
+ * @return double
+ */
+double StreamMath::VumetroIz()
+{
+    //double level;
+    int level;
+    level=BASS_ChannelGetLevel(stream);
+    return(ToDb(HIWORD(level)));
+}
 
-   double Segundos=Duracion();
+/**
+ * Returns the decibel
+ * @brief StreamMath::ToDb
+ * @param valor
+ * @return dobule
+ */
+double StreamMath::ToDb(double valor)
+{
+    double db=20*log(valor/100); //log esta en <math.h>
+    return(db);
+}
 
-    long pos=Segundos*1000;
-    long sec = pos/1000;
-    long min = sec/60;
-    long hour = min/60;
-    long msec = pos;
-    QTime *playTime = new QTime((int) hour%60, (int) min%60, (int) sec%60, (int) msec%1000);
+/**
+ * Convert formato (string) to secunds
+ * @brief StreamMath::FormatoToSegundos
+ * @param Formato
+ * @return double
+ */
+double StreamMath::FormatoToSegundos(QString Formato)
+{
+    QTime time = QTime::fromString(Formato, "HH:mm:ss"); // invalid
+    double Minutos, Segundos;
 
-     return(playTime->toString(Formato));
+    Minutos=time.hour()*60;
+    Minutos= Minutos + time.minute();
+    Segundos=Minutos *60 + time.second();
+    //qDebug() <<time.hour() <<time.minute() <<time.second();
 
-
+    return(Segundos);
 }
 
 
-QString StreamMath::SegundoToFormato(float Segundos,QString Formato){
-
+/**
+ * This function convert secunds (float) to formato (string)
+ * @brief StreamMath::SegundoToFormato
+ * @param Segundos
+ * @param Formato
+ * @return string
+ */
+QString StreamMath::SegundoToFormato(float Segundos,QString Formato)
+{
    long pos=Segundos*1000;
    long sec = pos/1000;
    long min = sec/60;
    long hour = min/60;
    long msec = pos;
+
    QTime *playTime = new QTime((int) hour%60, (int) min%60, (int) sec%60, (int) msec%1000);
 
-    return(playTime->toString(Formato));
-
+   return(playTime->toString(Formato));
 }
 
-
-QString StreamMath::SegundoToFormato(){
-
+/**
+ * This function convert secunds to string
+ * @brief StreamMath::SegundoToFormato
+ * @return string
+ */
+QString StreamMath::SegundoToFormato()
+{
     float Segundos= Duracion();
     long pos=Segundos*1000;
     long sec = pos/1000;
@@ -91,57 +153,26 @@ QString StreamMath::SegundoToFormato(){
     long msec = pos;
     QTime *playTime = new QTime((int) hour%60, (int) min%60, (int) sec%60, (int) msec%1000);
 
-     return(playTime->toString("hh:mm:ss:zzz"));
-
-
+    return(playTime->toString("hh:mm:ss:zzz"));
 }
 
+/**
+ * @brief StreamMath::SegundoToFormato
+ * @param Formato
+ * @return string
+ */
+QString StreamMath::SegundoToFormato(QString Formato)
+{
+    double Segundos=Duracion();
 
-//****************Nivel de vumetro derecho ***********************
+    long pos=Segundos*1000;
+    long sec = pos/1000;
+    long min = sec/60;
+    long hour = min/60;
+    long msec = pos;
 
-double StreamMath::VumetroDe(){
-   //double level;
-   int level;
-    level=BASS_ChannelGetLevel(stream);
-    return(ToDb(LOWORD(level)));
+    QTime *playTime = new QTime((int) hour%60, (int) min%60, (int) sec%60, (int) msec%1000);
 
+    return(playTime->toString(Formato));
 }
-//****************Nivel vumetro izquierdo*********************
-
-double StreamMath::VumetroIz(){
-   //double level;
-   int level;
-    level=BASS_ChannelGetLevel(stream);
-      return(ToDb(HIWORD(level)));
-
-}
-
-//************Retorna los decibelio **********************
-double StreamMath::ToDb(double valor){
-
-double db=20*log(valor/100); //log esta en <math.h>
-  return(db);
-}
-
-///////////////////////////////////////////////////////////////
-double StreamMath::FormatoToSegundos(QString Formato){
-
-  QTime time = QTime::fromString(Formato, "HH:mm:ss"); // invalid
-
-  double Minutos, Segundos;
-
-  Minutos=time.hour()*60;
-  Minutos= Minutos + time.minute();
-  Segundos=Minutos *60 + time.second();
-
-  //qDebug() <<time.hour() <<time.minute() <<time.second();
-  return(Segundos);
-
-
-}
-
-
-
-
-
 
--- engine/StreamMath.h
+++ engine/StreamMath.h
@@ -1,85 +1,43 @@
-/****************************************************************************
- **
- ** Copyright (C) 2010 Victor Algaba .
- ** All rights reserved.
- ** Contact: (www.radit.org)
- **
- ** *************************************************************************/
-
-
-
-
 /**
-   Esta clase da soporte al sistema matemático especificos para los ficheros de audio.
-
-
+ * StreamMath
+ * -----------------------------------------
+ *
+ * - This class supports the mathematical system of the audio file.
+ *
+ * @author Victor Algaba
  */
-
-
-
-
-
-
-
-
-
-
-
 #ifndef STREAMMATH_H
 #define STREAMMATH_H
 
 #include <QString>
-
 #include "bass.h"
-
-
 
 class StreamMath
 {
+    private:
+        HSTREAM stream;
+        double ToDb(double valor);
 
-private:
-    HSTREAM stream;
+    public:
+        StreamMath(HSTREAM stream);
+        StreamMath();
+        virtual ~StreamMath(){;}
 
-    double ToDb(double valor);
+        void SetStream(HSTREAM stream);
+        double Duracion();//get duration in seconds
+        double Trascurridos();//get time in progress
+        double Contador();//Returns the duration in seconds elapsed minus
 
-public:
+        /** Combierte segundos a formato "hh:mm:ss:zzz */
+        QString SegundoToFormato();
+        QString SegundoToFormato(QString Formato);
+        QString SegundoToFormato(float Segundos,QString Formato="hh:mm:ss:zzz");
 
+        double VumetroDe();//get level of DB of channel right
+        double VumetroIz();//get level of DB of channel left
 
-    StreamMath(HSTREAM stream);
-    StreamMath();
-
-    virtual ~StreamMath(){;}
-
- /** Establece el stream*/
-    void SetStream(HSTREAM stream);
-
-
-    /** Retorna la duracion en segundos*/
-      double Duracion();
-
-    /** Retorna el tiempo trascurrido en segundos */
-      double Trascurridos();
-
-
-    /** Retorna la duracion menos el trascurrido en segundos*/
-      double Contador();
-
-    /** Combierte segundos a formato "hh:mm:ss:zzz */
-      QString SegundoToFormato();
-      QString SegundoToFormato(QString Formato);
-      QString SegundoToFormato(float Segundos,QString Formato="hh:mm:ss:zzz");
-
-
-     /** Retorna el nivel de DB(decibelios) del canal derecho */
-     double VumetroDe();
-     /** Retorna el nivel de DB(decibelios) del canal Izquierdo */
-     double VumetroIz();
-
-     double FormatoToSegundos(QString Formato);
-
-
+        double FormatoToSegundos(QString Formato);
 };
-
 #endif // STREAMMATH_H
 
 
