3501835f7d0e2155caeccaf1b1bf3b6b08ab8c1a
caio_th <caio_th@yahoo.com.br>
2012-12-31 18:06:46 +0000

    Comments AddRadioInternet and ioLista.

--- AddRadioInternet.h
+++ AddRadioInternet.h
@@ -1,39 +1,41 @@
+/**
+ * AddRadioInternet
+ * -----------------------------------------
+ *
+ * - Control with views
+ * - Work with radio online
+ *
+ * @author Victor Algaba
+ */
 #ifndef ADDRADIOINTERNET_H
 #define ADDRADIOINTERNET_H
-
 #include <QUrl>
 #include "engine/StreamFile.h"
 #include "ui_FrmAddRadioInternet.h"
 
-
 class AddRadioInternet : public QDialog, public Ui::FrmAddRadioInternet
- {
-     Q_OBJECT
+{
+    Q_OBJECT
 
- private:
- HSTREAM cual;
- public:
-     explicit AddRadioInternet(QWidget *parent = 0);
-     virtual ~AddRadioInternet();
-     QString Url;
-     QString Duracion;
-     QString Nombre;
-     int Dispositivo;
+    private:
+        HSTREAM cual;
 
-public slots:
-     void ClickAceptar();
+    public:
+        explicit AddRadioInternet(QWidget *parent = 0);
+        virtual ~AddRadioInternet();
+        QString Url;
+        QString Duracion;
+        QString Nombre;
+        int Dispositivo;
 
+    public slots:
+        void ClickAceptar();
 
-private slots:
- void ClickPlay();
- void ClickStop();
- void ClickCancelar();
-signals:
+    private slots:
+        void ClickPlay();
+        void ClickStop();
+        void ClickCancelar();
 
-
-
- };
-
-
-
+    signals:
+};
 #endif // ADDRADIOINTERNET_H
